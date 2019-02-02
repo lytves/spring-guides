31c60eb4d052a241f21b12640f4717e1037e2a31
lytves <22356770+lytves@users.noreply.github.com>
2019-02-02 16:35:34 +0100

    names of method edited

--- accessing-data-mongodb/src/main/java/hello/CustomerRepository.java
+++ accessing-data-mongodb/src/main/java/hello/CustomerRepository.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
-    Customer findByFirstName(String firstName);
-    List<Customer> findByLastName(String lastName);
+    Customer findOneByFirstName(String firstName);
+    List<Customer> findAllByLastName(String lastName);
 }
