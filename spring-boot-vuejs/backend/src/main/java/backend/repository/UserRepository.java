2eecbb3ed7f866638ae123ab19d74200af3538f6
lytves <22356770+lytves@users.noreply.github.com>
2019-02-03 17:46:00 +0100

    UserRepository created

---
+++ spring-boot-vuejs/backend/src/main/java/backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package backend.repository;
+
+import backend.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByLastName(@Param("lastname") String lastname);
+
+    List<User> findByFirstName(@Param("firstname") String firstname);
+
+}
