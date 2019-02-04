a62ae1bd347b6827724a570b2390fb223fb4a620
lytves <22356770+lytves@users.noreply.github.com>
2019-02-04 20:59:50 +0100

    app about page edited

--- spring-boot-vuejs/frontend/src/views/About.vue
+++ spring-boot-vuejs/frontend/src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About project:</h1>
+    <p>
+      It's small basic Java web application that is using Spring Boot like a backend and new modern JavaScript framework Vue.js
+      like a frontend part. I tried to combine them and I believe it worked fine, it's a good choice to make some
+      modern light web applications. So try it, I am sure you would like it! Good luck!
+    </p>
   </div>
 </template>
