5cfdcd9364daf1ac5ad28bc19621224eda5849e0
lytves <22356770+lytves@users.noreply.github.com>
2019-02-04 21:45:17 +0100

    all Vue components moved to common components directory

--- spring-boot-vuejs/frontend/src/router.js
+++ spring-boot-vuejs/frontend/src/router.js
@@ -1,31 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Service from './views/Service.vue'
+import Home from '@/components/Home.vue'
+import Service from '@/components/Service.vue'
+import About from '@/components/About.vue'
+import User from '@/components/User.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: About
+      },
+      {
       path: '/callservice',
       name: 'service',
       component: Service
-    }
-  ]
+      },
+      {
+      path: '/user',
+      name: 'user',
+      component: User
+      }
+    ]
 })
